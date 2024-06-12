b97fe692c7b6e377fc73c269e87b766fe38267cf
ayyappa44488 <ayyappa44488@AYYAPPA-LAPTOP>
2024-06-12 22:31:21 +0530

    update

--- app.py
+++ app.py
@@ -2,15 +2,9 @@ from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-# hostname = 'localhost'
-# username = 'root'
-# password = 'Ayyappa2003'
-# port=3306
-# database = 'carddesign'
+from confidential import mysql_url
 
-# cnx=create_engine(f'mysql+pymysql://{username}:{password}@{hostname}:{port}/{database}')
-# conn=cnx.connect()
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:Ayyappa2003@localhost:3306/carddesign'
+app.config['SQLALCHEMY_DATABASE_URI']=mysql_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
